c3247f343b00e72e5b62ba9da444ef0a36aa830b
josh1506 <jmjabor@gmail.com>
2024-11-07 16:53:12 +0800

    [FastAPI Request Method Logic] Create, update and delete request

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -44,3 +44,21 @@ async def read_books_by_author(book_author: str, category: str):
         if is_book_author and is_book_category:
             book_list.append(book)
     return book_list
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    return new_book
+
+
+@app.put("/books/update/{book_id}")
+async def update_book(book_id: int, new_book=Body()):
+    BOOKS[book_id] = new_book
+    return "Book updated successfully"
+
+
+@app.delete("/books/delete/{book_id}")
+async def delete_book(book_id: int):
+    del BOOKS[book_id]
+    return "Book deleted successfully"
