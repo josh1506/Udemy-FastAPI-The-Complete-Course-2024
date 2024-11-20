44df1d0c33e2566cf85391904793d52344b332ee
josh1506 <jmjabor@gmail.com>
2024-11-20 12:46:46 +0800

    [Unit And Integration Testing] Test create todo

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -84,3 +84,22 @@ def test_read_one_authenticated_not_found(test_todo):
     response = client.get("/todo/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {"detail": "Todo not found."}
+
+
+def test_create_todo(test_todo):
+    request_data = {
+        "title": "New todo",
+        "description": "New todo description",
+        "priority": 5,
+        "complete": False,
+    }
+
+    response = client.post("/todo/create", json=request_data)
+    assert response.status_code == status.HTTP_201_CREATED
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 2).first()
+    assert model.title == request_data.get("title")
+    assert model.description == request_data.get("description")
+    assert model.priority == request_data.get("priority")
+    assert model.complete == request_data.get("complete")
