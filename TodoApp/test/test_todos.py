450585ad07b5d748d1463acabb99f16ee50dd7b6
josh1506 <jmjabor@gmail.com>
2024-11-20 12:37:39 +0800

    [Unit And Integration Testing] Setup pytest fixture

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,12 +1,14 @@
 from fastapi import status
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+import pytest
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
 from routers.todos import get_db, get_current_user
 from database import Base
 from main import app
+from models import Todos
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
 
@@ -36,7 +38,32 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 
 client = TestClient(app)
 
-def test_real_all_authenticated():
+@pytest.fixture
+def test_todo():
+    todo = Todos(
+        title="Learn to code",
+        description="Need to learn everyday!",
+        priority=5,
+        complete=False,
+        owner_id=1,
+    )
+
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield db
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM todos;"))
+        connection.commit()
+
+def test_real_all_authenticated(test_todo):
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == []
+    assert response.json() == [{
+        "id": 1,
+        "title": "Learn to code",
+        "description": "Need to learn everyday!",
+        "priority": 5,
+        "complete": False,
+        "owner_id": 1,
+    }]
