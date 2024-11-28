52e29c0a4ed0dde11a06b0f1ce0dbc57039c1934
josh1506 <jmjabor@gmail.com>
2024-11-28 12:54:06 +0800

    [Unit And Integration Testing] Moved reusable functions to utils.py for better code organization.

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,60 +1,10 @@
 from fastapi import status
-from fastapi.testclient import TestClient
-import pytest
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
 
-from routers.todos import get_db, get_current_user
-from database import Base
-from main import app
-from models import Todos
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool,
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def override_get_current_user():
-    return {"username": "sample", "id": 1, "user_role": "admin"}
-
+from .utils import *
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
 
-client = TestClient(app)
-
-@pytest.fixture
-def test_todo():
-    todo = Todos(
-        title="Learn to code",
-        description="Need to learn everyday!",
-        priority=5,
-        complete=False,
-        owner_id=1,
-    )
-
-    db = TestingSessionLocal()
-    db.add(todo)
-    db.commit()
-    yield db
-    with engine.connect() as connection:
-        connection.execute(text("DELETE FROM todos;"))
-        connection.commit()
 
 def test_read_all_authenticated(test_todo):
     response = client.get("/")
@@ -68,6 +18,7 @@ def test_read_all_authenticated(test_todo):
         "owner_id": 1,
     }]
 
+
 def test_read_one_authenticated(test_todo):
     response = client.get("/todo/1")
     assert response.status_code == status.HTTP_200_OK
@@ -79,6 +30,7 @@ def test_read_one_authenticated(test_todo):
         "complete": False,
         "owner_id": 1,
     }
+
 
 def test_read_one_authenticated_not_found(test_todo):
     response = client.get("/todo/999")
